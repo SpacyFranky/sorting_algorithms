a0e24e81391e6a679aecc1bc858f4f594fd4f075
taiebchaabini <taiebchaabini@gmail.com>
2020-02-05 09:46:03 +0000

    Betty coding style fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,22 +7,22 @@
  **/
 void bubble_sort(int *array, size_t size)
 {
-    unsigned int tmp = 0, i = 0, b = 0;
+	unsigned int tmp = 0, i = 0, b = 0;
 
-    if (!size)
-        return;
+	if (!size)
+		return;
 
-    for (b = 0; b < size; b++)
-    {
-        for(i = 0; i < size; i++)
-        {
-            if (array[i + 1] && array[i] > array[i + 1])
-            {
-                tmp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = tmp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (b = 0; b < size; b++)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (array[i + 1] && array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
