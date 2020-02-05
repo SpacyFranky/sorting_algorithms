6bd8a0ae35591be4228bff2d76f31eabd69832c6
taiebchaabini <taiebchaabini@gmail.com>
2020-02-05 09:29:29 +0000

    My new first commits

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+ * bubble_sort - function that sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ * @array: input
+ * @size: size of the array
+ **/
+void bubble_sort(int *array, size_t size)
+{
+    unsigned int tmp = 0, i = 0, b = 0;
+
+    for (b = 0; b < size; b++)
+    {
+        for(i = 0; i < size; i++)
+        {
+            if (array[i + 1] && array[i] > array[i + 1])
+            {
+                tmp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = tmp;
+                print_array(array, size);
+            }
+        }
+    }
+}
