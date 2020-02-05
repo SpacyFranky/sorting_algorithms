ce1341d1658499fe296de46f8bdc67224705aa2d
SpacyFranky <bigaziz@outlook.com>
2020-02-05 21:02:49 +0000

    Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- *
+ * partition - Lomuto partition scheme.
+ * @array: the array that will be sorted.
+ * @l: index of smaller element.
+ * @h: index of last element.
+ * @size: size of the array.
+ * Return: index i.
  */
-size_t partition(int *array, size_t l, size_t h, size_t size)
+size_t partition(int *array, int l, int h, size_t size)
 {
-	int pivot, x, y;
-	size_t i, j;
+	int i, j, pivot, x, y;
 
 	pivot = array[h];
 	i = l;
@@ -18,9 +19,13 @@ size_t partition(int *array, size_t l, size_t h, size_t size)
 	{
 		if (pivot > array[j])
 		{
-			x = array[i];
-			array[i] = array[j];
-			array[j] = x;
+			if (i != j)
+			{
+				x = array[i];
+				array[i] = array[j];
+				array[j] = x;
+				print_array(array, size);
+			}
 			i++;
 		}
 	}
@@ -32,30 +37,36 @@ size_t partition(int *array, size_t l, size_t h, size_t size)
 }
 
 /**
- *
- *
- *
+ * quick - sorts an array of integers in ascending order
+ * using the Quick sort algorithm.
+ * @array: the array that will be sorted.
+ * @l: index of smaller element.
+ * @h: index of last element.
+ * @size: size of the array.
  */
-void quick(int *array, size_t l, size_t h, size_t size)
+void quick(int *array, int l, int h, size_t size)
 {
 	size_t p;
 
-        if (l < h)
-        {
-                p = partition(array, l, h, size);
+	if (l < h)
+	{
+		p = partition(array, l, h, size);
 		quick(array, l, p - 1, size);
-                quick(array, p + 1, h, size);
+		quick(array, p + 1, h, size);
 	}
 }
 
 
 /**
  * quick_sort - sorts an array of integers in ascending order
- * using the Quick sort algorithm
+ * using the Quick sort algorithm.
  * @array: the array that will be sorted.
  * @size: size of the array.
  */
 void quick_sort(int *array, size_t size)
 {
+	if (array == NULL)
+		return;
+
 	quick(array, 0, size - 1, size);
 }
