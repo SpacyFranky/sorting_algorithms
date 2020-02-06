05a42d23cc41eaf7b7396cc801d593324a265c16
taiebchaabini <taiebchaabini@gmail.com>
2020-02-06 20:24:46 +0000

    Correction to algo + good output

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,7 +4,7 @@
  * insertion_sort_list - function that sorts a doubly linked list of integers
  * in ascending order using the Insertion sort algorithm
  * @list: input of double linked list
-**/
+ **/
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *tmp = NULL, *head = NULL, *prevtmp = NULL, *nexttmp = NULL;
@@ -13,23 +13,35 @@ void insertion_sort_list(listint_t **list)
 	tmp = *list;
 	while (tmp)
 	{
-		if (tmp->next && tmp->n > tmp->next->n)
+		if (tmp->prev && tmp->prev == head && tmp->prev->n > tmp->n)
 		{
-			/**
-			 * [] - [node] - [next-node] - []
-			 *
-			 **/
 			nexttmp = tmp->next;
-			tmp->next = tmp->next->next;
 			prevtmp = tmp->prev;
-			/* next node in prev node */
+			tmp->next = prevtmp;
+			tmp->prev = NULL;
+			prevtmp->next = nexttmp;
+			prevtmp->prev = tmp;
+			nexttmp->prev = prevtmp;
+			head = tmp;
+			tmp = head;
+			print_list(tmp);
+		}
+
+		else if (tmp->next && tmp->n > tmp->next->n)
+		{
+			nexttmp = tmp->next;
+			prevtmp = tmp->prev;
+			tmp->next = tmp->next->next;
 			tmp->prev = nexttmp;
 			nexttmp->next = tmp;
 			nexttmp->prev = prevtmp;
+			if (nexttmp->next->next)
+				nexttmp->next->next->prev = tmp;
 			prevtmp->next = nexttmp;
 			tmp = head;
 			print_list(tmp);
 		}
-		tmp = tmp->next;
+			tmp = tmp->next;
 	}
+	*list = head;
 }
